b1d1ed2914571a642a75080f12d47dfcd82734a2
onetail <paosong91@gmail.com>
2021-05-18 19:56:06 +0800

    test: a =1

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -4,10 +4,9 @@ export default class HomeController extends Controller {
   public async index() {
     const { ctx } = this;
 
-
-
     const a = 1;
-    console.log(a)
+
+    console.log(a);
     ctx.body = await ctx.service.test.sayHi('egg');
   }
 }
